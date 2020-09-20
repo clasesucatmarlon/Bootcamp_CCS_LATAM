fa4c8836d0c4245d974b1b6204bf80b1211ff207
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-20 16:21:17 -0500

    add img backgrund, css and methods for operations

--- js/calc.js
+++ js/calc.js
@@ -5,6 +5,10 @@ const buttonEqual = document.getElementsByName('Data-equal')[0];
 const buttonDeleted = document.getElementsByName('Data-deleted')[0];
 var result = document.getElementById("result");
 
+var operationCurrent = '';
+var operationPrevious = '';
+var operations = undefined;
+
 // Add events to elements
 buttonNumbers.forEach(function (button) { // call element fot array of number buttons
     button.addEventListener('click', function () { // call function for add number
@@ -21,10 +25,63 @@ buttonOperations.forEach(function (button) {
 
 buttonEqual.addEventListener('click', function () {
     calculate();
-    actualizarDisplay();
+    updateDisplay();
 })
 
 buttonDeleted.addEventListener('click', function () {
     clear();
-    actualizarDisplay();
+    updateDisplay();
 })
+
+function addNumber(num) {
+    operationCurrent = operationCurrent.toString() + num.toString();
+    updateDisplay();
+}
+
+function updateDisplay() {
+    result.value = operationCurrent;
+}
+
+function clear() {
+    operationCurrent = '';
+    operationPrevious = '';
+    operations = undefined;
+}
+
+clear();
+
+function selectOperation(oper) {
+    if (operationCurrent === '') return;
+    if (operationPrevious !== '') {
+        calculate();
+    }
+    operations = oper.toString();
+    operationPrevious = operationCurrent;
+    operationCurrent = '';
+}
+
+function calculate() {
+    var calc;
+    const previous = parseFloat(operationPrevious);
+    const current = parseFloat(operationCurrent);
+    if (isNaN(previous) || isNaN(current)) return;
+    switch (operations) {
+        case '+':
+            calc = previous + current;
+            break;
+        case '-':
+            calc = previous - current;
+            break;
+        case 'x':
+            calc = previous * current;
+            break;
+        case '/':
+            calc = previous / current;
+            break;
+        default:
+            return;
+    }
+    operationCurrent = calc;
+    operations = undefined;
+    operationPrevious = '';
+}
